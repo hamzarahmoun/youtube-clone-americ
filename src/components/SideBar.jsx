2fc42af96e0ede4cfdcd308847f728c226b65271
hamzarahmoun <hamza.rahmoun456@gmail.com>
2023-09-17 11:28:13 +0100

    first commit

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,40 @@
+import { Stack } from '@mui/material'
+import React from 'react'
+import { categories } from '../utils/constants'
+
+const SideBar = ({selectedCategory,setselectedCategory}) => {
+  return (
+    <Stack
+    
+    direction="row"
+    sx={{
+      overflowY: "auto",
+      height: { sx: "auto", md: "95%" },
+      flexDirection: { md: "column" },
+    }}
+    >
+        {categories.map((Category)=>(
+            <button className='category-btn'
+            onClick={()=>setselectedCategory(Category.name)}
+            style={{
+                background: Category.name===
+                selectedCategory && '#FC1503',
+                color:'white'
+            
+            }}
+            key={Category.name}
+            
+            >
+                <span style={{ color: Category.name === selectedCategory ? "white" : "red", marginRight: "15px" }}>
+          {Category.icon}</span>
+                <span style={{ opacity: Category.name === selectedCategory ? "1" : "0.8" }} >{Category.name}</span>
+            </button>
+        ))
+        
+        }
+
+    </Stack>
+  )
+}
+
+export default SideBar
